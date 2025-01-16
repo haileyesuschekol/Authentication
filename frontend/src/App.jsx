0081aa736962919879e0d01388e8209e33dc1c4f
Haileyesus Chekol <teamdevs7@gmail.com>
2025-01-16 22:47:53 +0000

    create route redirects

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,71 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import { Toaster } from "react-hot-toast"
 import Signup from "./pages/Signup"
 import Login from "./pages/Login"
 import EmailVerification from "./components/EmailVerification"
+import { useAuthStore } from "./store/authStore"
+import { useEffect } from "react"
+import Home from "./pages/Home"
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, user } = useAuthStore()
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />
+  }
+
+  if (!user.isVerified) {
+    return <Navigate to="/verify-email" replace />
+  }
+
+  return children
+}
+
+// redirect authenticated users to the home page
+const RedirectAuthUserToHome = ({ children }) => {
+  const { isAuthenticated, user } = useAuthStore()
+
+  if (isAuthenticated && user.isVerified) {
+    return <Navigate to="/" replace />
+  }
+
+  return children
+}
 
 function App() {
+  const { isCheckingAuth, checkAuth, isAuthenticated, user } = useAuthStore()
+
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth])
+
   return (
     <div className=" min-h-screen flex items-center justify-center relative overflow-hidden">
       <Routes>
-        <Route path="/" element={"Home"} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <RedirectAuthUserToHome>
+              <Signup />
+            </RedirectAuthUserToHome>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RedirectAuthUserToHome>
+              <Login />
+            </RedirectAuthUserToHome>
+          }
+        />
         <Route path="/verify-email" element={<EmailVerification />} />
       </Routes>
       <Toaster />
