b2e4d8e545a1b5eb34df28af8676f6592bd9d054
Haileyesus Chekol <teamdevs7@gmail.com>
2025-01-17 03:37:15 +0000

    update url for production

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,10 @@
 import { create } from "zustand"
 import axios from "axios"
 
-const Api_URL = "http://localhost:5000/api/v1/auth"
+const Api_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api/v1/auth"
+    : "/api/auth"
 axios.defaults.withCredentials = true
 
 export const useAuthStore = create((set) => ({
